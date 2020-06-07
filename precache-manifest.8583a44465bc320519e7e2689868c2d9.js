80678e84222fb145ed484d12dc722422e16039f5
jack007xx <jack007xx@gmail.com>
2020-06-08 04:14:05 +0900

    Updates

---
+++ precache-manifest.8583a44465bc320519e7e2689868c2d9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c472df8c42f83294f6b331f39007ab4b",
+    "url": "/kusachu-translate/index.html"
+  },
+  {
+    "revision": "6a6433630097cfadcfd4",
+    "url": "/kusachu-translate/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e4a1d056163452f6f551",
+    "url": "/kusachu-translate/static/js/2.6eced127.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/kusachu-translate/static/js/2.6eced127.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a6433630097cfadcfd4",
+    "url": "/kusachu-translate/static/js/main.4cc53e0b.chunk.js"
+  },
+  {
+    "revision": "dc55f23518a9636b913e",
+    "url": "/kusachu-translate/static/js/runtime-main.b7a062ff.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/kusachu-translate/static/media/logo.5d5d9eef.svg"
+  }
+]);
