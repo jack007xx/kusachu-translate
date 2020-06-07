14696d83e0d46cbd20c7bb75c822be03940813cc
jack007xx <jack007xx@gmail.com>
2020-06-08 07:19:36 +0900

    とりあえず

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,31 +1,68 @@
 import React from "react";
 import { useState } from "react";
-import { TextField } from "@material-ui/core";
-import kusachuEngine from "../functions/kusachuEngine";
-import ArrowDownwardIcon from '@material-ui/icons/ArrowDownward';
+import {
+  TextField,
+  Box,
+  CssBaseline,
+  MuiThemeProvider,
+  createMuiTheme,
+  makeStyles,
+  Theme,
+  createStyles,
+} from "@material-ui/core";
+import kusachuEngine from "./kusachuEngine";
+import ArrowDownwardIcon from "@material-ui/icons/ArrowDownward";
+import cyan from "@material-ui/core/colors/cyan";
+
+const theme = createMuiTheme({
+  palette: {
+    primary: cyan,
+  },
+});
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    field: {
+      width: "960px",
+      margin: "auto",
+    },
+  })
+);
 
 const App: React.FC = () => {
   const [inText, setInText] = useState("");
   const [kusachuText, setKusachuText] = useState("");
+  const classes = useStyles();
   const handleOnChangeOriginal = (e: React.ChangeEvent<HTMLInputElement>) => {
     setInText(e.target.value);
-    setKusachuText(kusachuEngine(inText));
+    setKusachuText(kusachuEngine(e.target.value));
   };
   return (
     <>
-      <TextField
-        label="原文"
-        multiline
-        rows={5}
-        variant="outlined"
-        onChange={handleOnChangeOriginal}
-      />
-      <ArrowDownwardIcon />
-      <TextField label="クサチュー語"
-        multiline
-        rows={5}
-        variant="outlined"
-        value={kusachuText} />
+      <MuiThemeProvider theme={theme}>
+        {/* <CssBaseline /> */}
+        <Box>
+          <TextField
+            label="原文"
+            multiline
+            rows={5}
+            variant="outlined"
+            onChange={handleOnChangeOriginal}
+            className={classes.field}
+          />
+          {/* <div>
+            <ArrowDownwardIcon />
+          </div> */}
+          <TextField
+            label="クサチュー語"
+            multiline
+            rows={5}
+            variant="outlined"
+            className={classes.field}
+            value={kusachuText}
+          />
+        </Box>
+      </MuiThemeProvider>
     </>
   );
 };
--- webpack.config.js
+++ webpack.config.js
@@ -21,7 +21,7 @@ module.exports = {
     ],
   },
   resolve: {
-    extensions: [".tsx", "ts", ".js"],
+    extensions: [".tsx", ".ts", ".js"],
   },
   devtool: "source-map",
   devServer: {
